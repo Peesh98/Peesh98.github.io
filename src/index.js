31cbff2ef947eba7e324d1234c9384aa001ef5f5
Peesh98 <timothy.t.le98@gmail.com>
2024-01-20 04:01:13 +0000

    Changed BroswerRouter to HashRouter for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { CookiesProvider } from 'react-cookie';
